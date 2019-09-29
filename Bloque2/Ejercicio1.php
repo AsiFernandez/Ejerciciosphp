74fd7f9efd81df986d3a4321d6449f52882662e1
Asier Fernandez <afernandezce17dw@ikzubirimanteo.com>
2019-09-29 17:56:05 +0200

    Cambios en los titulos de los ejercicios

--- Bloque2/Ejercicio1.php
+++ Bloque2/Ejercicio1.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Funcion 1</title>
+	<title>Ejercicio 1</title>
 </head>
 <body>
 	
 	 
 <?php
-		$numNaturales = [0,1,2,3,4,5,6,7,8,9,10];
+		$numNaturales = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 		$factorial=[];
 			function factorial($n){
 		    	if ($n <= 1) {
@@ -21,7 +21,7 @@
 			echo $numero." el factorial es: ".$factorial[$numero]."</br>";
 		}
 		
-	?>
+?>
 
 </body>
 </html>
--- Bloque2/Ejercicio2.php
+++ Bloque2/Ejercicio2.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Funcion 1</title>
+	<title>Ejercicio 2</title>
 </head>
 <body>
 	
 	 
 <?php
 
-	$filas = rand(1, 10);
-	$columnas = rand(1, 10)	;
+	$filas = 3;
+	$columnas = 5	;
 
 	function crearTabla ($param1, $param2){
 		
-		for ($i=0; $i <= $param1; $i++) { 
+		for ($i=1; $i <= $param1; $i++) { 
 		echo  "<tr style='border:1px solid;' > $i";
-		for ($j= 0; $j <= $param2 ; $j++) { 
+		for ($j=1; $j <= $param2 ; $j++) { 
 			echo  "<td style='border:1px solid;' > $j </td>";
 		}
 		echo  "</tr></br>";
