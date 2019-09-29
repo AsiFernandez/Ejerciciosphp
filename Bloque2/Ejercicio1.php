6f096c2ae69c47a0eb68fde5a38b43438127241b
Asier Fernandez <afernandezce17dw@ikzubirimanteo.com>
2019-09-29 16:43:02 +0200

    Creado el ejercicio 1 pero no se resolverlo

---
+++ Bloque2/Ejercicio1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Funcion 1</title>
+</head>
+<body>
+	
+	 
+<?php
+		$numNaturales = [0,1,2,3,4,5,6,7,8,9,10];
+		$factorial=[];
+			function factorial($n){
+		    	if ($n <= 1) {
+		        	return 1;
+		    	}
+		 
+		    	return $n * factorial($n - 1);
+			}
+		foreach ($numNaturales as $numero){
+			$factorial[$numero] = factorial($numero);
+			echo $numero." el factorial es: ".$factorial[$numero]."</br>";
+		}
+		
+	?>
+
+</body>
+</html>
