ce481c1c0c0746de31a17f00fb071d21426f9f6c
Asier Fernandez <afernandezce17dw@ikzubirimanteo.com>
2019-09-30 20:59:28 +0200

    Ejercicio 4 terminado

--- Bloque2/Ejercicio4.php
+++ Bloque2/Ejercicio4.php
@@ -7,12 +7,14 @@
 <?php
 	
 	$frase = "perro gato conejo raton cabra";
-	$palabras = explode(" ", $frase);
+	$palabras = [];
+	$aAsociativo = explode(" ", $frase);
 
-	for ($i=0; $i < count($palabras); $i++) { 
-		echo  " " . $palabras[$i]. "=>" . strlen($palabras[$i] );
+	for ($i=0; $i <= count($palabras); $i++) { 
+		$palabras[$aAsociativo[$i]] = strlen($aAsociativo[$i] );
 	}
-	
+
+	print_r($palabras);
 ?>
 </body>
 </html>
