61f317e9b86d9700a5804f78d3ba83232ae53b83
Asier Fernandez <afernandezce17dw@ikzubirimanteo.com>
2019-09-30 22:42:21 +0200

    Ejercicio 5 hecho y finalizado

---
+++ Bloque2/Ejercicio5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio 5</title>
+</head>
+<body>
+
+	<?php
+		$mesesDias = [ "Enero" =>31, "Febrero" =>28, "Marzo" =>31, "Abril" =>30, "Mayo" =>31, "Junio" =>31, "Julio" =>31, "Agosto" =>31, "Septiembre" =>30, "Octubre" =>31, "Noviembre" =>30, "Diciembre" =>31]; 
+
+		print_r($mesesDias);
+	?>
+
+</body>
+</html>
