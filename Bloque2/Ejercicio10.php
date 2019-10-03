f7d51455ba314e301ea41376453c49d12a9c465c
AsiFernandez <afernandezce17dw@ikzubirimanteo.com>
2019-10-03 09:22:11 +0200

    Ejercicio 10 resuelto

--- Bloque2/Ejercicio10.php
+++ Bloque2/Ejercicio10.php
@@ -14,41 +14,48 @@
             private $dni = '';
             
             //Constructor con los parametros de la persona
-            public function __construct($nombre, $apellido, $dni) {
-                $this->nombre = $nombre;
-                $this->apellido = $apellido;
-                $this->dni = $dni;
-            }
+	            public function __construct($nombre, $apellido, $dni) {
+	                $this->nombre = $nombre;
+	                $this->apellido = $apellido;
+	                $this->dni = $dni;
+	            }
 
             //Metodos get y set de cada atributo
-            public function setNombre($nombre) {
-                $this->nombre = $nombre;
-            }
-            public function getNombre() {
-                echo $this->nombre;
-            }
-            public function setApellido($apellido) {
-                $this->apellido = $apellido;
-            }
-            public function getApellido() {
-                echo $this->apellido;
-            }
-            public function setDni($dni) {
-                $this->dni = $dni;
-            }
-            public function getDni() {
-                echo $this->dni;
-            }
+	            public function setNombre($nombre) {
+	                $this->nombre = $nombre;
+	            }
+
+	            public function getNombre() {
+	                echo $this->nombre;
+	            }
+
+	            public function setApellido($apellido) {
+	                $this->apellido = $apellido;
+	            }
+
+	            public function getApellido() {
+	                echo $this->apellido;
+	            }
+
+	            public function setDni($dni) {
+	                $this->dni = $dni;
+	            }
+
+	            public function getDni() {
+	                echo $this->dni;
+	            }
 
             //Funcion para mostrar los datos en pantalla
             public function mostrarDatos() {
                 echo "Persona: ".$this->nombre." ".$this->apellido;
             }
         }
+            
             //clase usuario que extiende de persona
             class User extends Persona {
 				private $puntos = 0;
-				// Declaración de los métodos get y set
+
+				// Declaramos los metodos get y set para la clase persona
 				public function getPuntos() {
 					echo $this->puntos;
 				}
@@ -64,6 +71,7 @@
 				}
 			}
 		
+
 
 
 	?>
